46e3c1d7bf3a5dfa3566f5bbb257388496972059
Raouf Madani <raoufmadani@live.fr>
2020-05-31 14:06:29 +0100

    ToDoList App V2 with mongoDB using mongoose

--- app.js
+++ app.js
@@ -2,53 +2,171 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const app = express();
-const items = ["Buy food","Cook food","Eat food"];
-const workItems=[];
+const _=require("lodash");
+
 app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-app.get('/',function(req,res){
-   const today= new Date();
-   
-   const options= {
-       weekday:'long',
-       day:'numeric',
-       month:'long'
-   };
+//Connect our database
+mongoose.connect("mongodb+srv://raoufy-todolist:unbeaten40@todolistcluster-qhorl.mongodb.net/todolistDB",{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify: false});
 
-   const day = today.toLocaleDateString('en-US',options);
-   res.render("list",{listTitle:day,newListItems:items});
+//Schema
+const todolistSchema= new mongoose.Schema({
+   name:String
 });
 
-app.post('/',function(req,res){
-    const item = req.body.newItem;
+//Model
+const Item= mongoose.model("Item",todolistSchema);
 
-    if(req.body.list==="Work"){
-        workItems.push(item);
-        res.redirect('work');
-    }else{
-        // when the post is triggered in the home route, it will bring the input name and store it in item variable
-        //then it will redirect in the home route, it enters to app.get and render listItem >>> item
-        items.push(item);
+//collection
+const itemOne= new Item({
+    name:"Welcome to your ToDoListApp"
+});
+const itemTwo= new Item({
+    name:"Hit the + button to add new Item"
+});
+const itemThree= new Item({
+    name:"Hit the checkbox to delete an Item"
+});
+
+//Array to store collections
+const defaultItems=[itemOne,itemTwo,itemThree];
+
+//list schema
+const listSchema = {
+    name:String,
+    items:[todolistSchema]
+};
+//list model
+const List = mongoose.model("List",listSchema);
+
+//Insertion
+/*Item.insertMany(defaultItems,err=>{
+   if(err){
+       console.log(err);
+   }else{
+       mongoose.connection.close();
+       console.log("Insertion succeeded");
+   }
+  
+});*/
+
+
+
+app.get('/',(req,res)=>{
+  
+   //selection model==>render items from our db
+   Item.find((err,items)=>{
+    if(items.length === 0){
+        //Insertion model
+        Item.insertMany(defaultItems,err=>{
+            if(err){
+                console.log(err);
+            }else{
+                console.log("Insertion succeeded");
+            }  
+        });
         res.redirect('/');
+    }else{
+        res.render("list",{listTitle:"Today",newListItems:items});
+    }   
+   });
+   
+});
+
+//Update an Item
+/*Item.updateOne({_id:"5ed22f1072c2b70b347513af"},
+                {name:"Hit the checkbox to delete an Item"},
+                err=>{
+                    if(err)
+                    {console.log(err);}else{console.log("successfully updated")}
+                });*/
+
+app.post('/',(req,res)=>{
+    const itemName = req.body.newItem;
+    const listName = req.body.list;
+
+    const item= new Item({
+        name:itemName
+    });
+
+    if(listName === "Today"){
+        item.save();
+        res.redirect("/");
+    }else{
+        List.findOne({name:listName},(err,foundList)=>{
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect("/"+listName); 
+        });
+    }
+      
+});
+
+app.post('/delete',(req,res) =>{
+    const checkedItemID=req.body.checkbox;
+    const listName= req.body.listName;
+    //Check which list
+    if(listName==="Today"){
+        Item.deleteOne({_id:checkedItemID},err=>{
+            if(!err){
+                console.log("deleted successfully");
+                res.redirect('/');
+            }
+        });
+    }else{
+       List.findOneAndUpdate({name:listName},{$pull:{items:{_id:checkedItemID}}},(err,foundList)=>{
+          if(!err){
+              res.redirect('/'+listName);
+          }
+       });
     }
     
+    /**
+     * Another Method to remove Item
+     * Item.findByIdAndRemove(checkedItemID,err=>{
+        if(!err){
+            console.log("deleted successfully");
+            res.redirect('/');
+        }
+     * });
+     */
 });
 
-app.get('/work',function(req,res){
-  res.render("list",{listTitle:"Work List",newListItems:workItems});
+app.get('/:customList',(req,res)=>{
+    const customListName = _.capitalize(req.params.customList);
+
+    List.findOne({name:customListName},(err,foundList)=>{
+        if(!err){
+            if(!foundList){
+                //Create a new List
+                const list = new List({
+                    name:customListName,
+                    items:defaultItems
+                });
+                list.save();
+                res.redirect("/"+customListName);
+            }else{
+                //Show an existing list
+                res.render("list",{listTitle:foundList.name,newListItems:foundList.items});
+            }
+        }
+    });
+
 });
 
-app.post('/work',function(req,res){
+app.post('/work',(req,res)=>{
     let item= req.body.newItem;
     workItems.push(item);
     res.redirect("/");
 });
 
-app.listen(3000,function(){
+
+
+app.listen(3000,()=>{
     console.log('Server has started!')
 });
