4f9af9bf541bcc431f76858d54adcf2458d261f7
Raouf Madani <raoufmadani@live.fr>
2020-05-31 14:25:05 +0100

    ToDoListApp V2 using Ejs, Nodejs, Lodash, MongooseJS, MongoDB and HTML/CSS

--- app.js
+++ app.js
@@ -165,8 +165,11 @@ app.post('/work',(req,res)=>{
     res.redirect("/");
 });
 
+let port= process.env.PORT;
+if(port=== null || port===""){
+    port=3000;
+}
 
-
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('Server has started!')
 });
